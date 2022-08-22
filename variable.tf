53de94d90328a042453efed3b7f3782372e88eb2
Amvion <111630300+Amvion@users.noreply.github.com>
2022-08-22 15:51:14 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,22 @@
+variable "alb-sg-name" {
+    type = string
+  
+}
+variable "alb-from-port-i1" {
+  type = string
+}
+
+variable "alb-to-port-i1" {
+  type = string
+}
+
+variable "alb-from-port-i2" {
+  type = string
+}
+
+variable "alb-to-port-i2" {
+  type = string
+}
+variable "vpc_name" {
+  type = string
+}
